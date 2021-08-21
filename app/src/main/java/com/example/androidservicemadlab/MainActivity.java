ed8195e5a4bdd276c474f207d51a8a73445bfbe4
Maleesha Fernando <maleeshafernando31@gmail.com>
2021-08-21 13:55:59 +0530

    first commit

---
+++ app/src/main/java/com/example/androidservicemadlab/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidservicemadlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        Button buttonStop = (Button) findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+    }
+}
